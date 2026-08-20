5a99cfdc331fb7673068663ffa1ebd7f0581062e
Kimchanghee <k931103@gmail.com>
2026-08-21 02:53:15 +0900

    chore(release): prepare v3.1.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("requirements.txt", "r", encoding="utf-8") as f:
 
 setup(
     name="ceo-thread-auto",
-    version="3.0.78",
+    version="3.1.0",
     description="Thread auto uploader",
     author="와이엠",
     python_requires=">=3.9",
